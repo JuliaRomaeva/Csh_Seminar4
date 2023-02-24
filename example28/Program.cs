4d57c751a6b79804d5a31f315f2dc1465013fc37
JuliaRomaeva <julia.romaeva@gmail.com>
2023-02-24 20:49:57 +0300

    факториал функция

---
+++ example28/Program.cs
@@ -0,0 +1,20 @@
+﻿//28 Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.
+//4-> 24
+//5-> 120 == 1 * 2 * 3 * 4 * 5
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int factorial(int n)
+{
+    int f = 1;
+    for (int i = 1; i <= n; i++)
+        f *= i;
+    // Console.WriteLine(i);
+    return f;
+}
+
+int result = factorial(num);
+Console.WriteLine($"{num} -> {result}");
